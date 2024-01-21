8303077fce6c6eec8e2c5821d4e69c3d1de1a533
roska <r.oskar.15@wp.pl>
2024-01-21 16:26:26 +0100

    Messages works, just don't test it too much

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { ThemeProvider } from '@/components/providers/theme-provider'
 import { cn } from '@/lib/utils'
 import { ModalProvider } from '@/components/providers/modal-provider'
 import { SocketProvider } from '@/components/providers/socket-provider'
+import { QueryProvider } from '@/components/providers/query-provider'
 const font  = Open_Sans({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -32,7 +33,9 @@ export default function RootLayout({
               enableSystem={false}
               storageKey='discord-theme'>
                 <ModalProvider/>
+                <QueryProvider>
                 {children}
+                </QueryProvider>
               </ThemeProvider>
             </SocketProvider>
         </body>
